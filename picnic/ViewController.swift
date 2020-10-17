736c0c6b9c7e4ffa51e0f3ebf1cf0714ecb50377
Dmitry Marinin <436910463q@gmail.com>
2020-10-17 16:04:23 +0300

    Initial Commit

---
+++ picnic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  picnic
+//
+//  Created by Дмитрий Маринин on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
